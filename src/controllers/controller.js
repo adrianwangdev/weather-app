6c21ca34e21659e674abcf4bc4528e0b6810010b
wry608 <wry608.work@gmail.com>
2021-01-04 22:53:29 +0800

    call weather api

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,5 +1,21 @@
+const axios = require('axios')
+const API_KEY = '4dab701cb8af0b1b9babc59e92aa5385'
+
 exports.renderHomePage = (req, res) => {
   res.render('index')
+}
+
+exports.getWeather = (req, res) => {
+  const city = req.body.city
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+
+  axios.get(url)
+    .then(response => {
+      console.log(`${response.data.name} 目前的溫度為 ${response.data.main.temp}`)
+    })
+    .catch(error => {
+      console.log(error)
+    })
 }
 
 exports.renderAboutPage = (req, res) => {
